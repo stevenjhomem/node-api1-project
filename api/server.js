1bfb34530e1a594cce2b55d85d3bac0c52711f58
stevenjhomem <sjhomem@outlook.com>
2021-09-27 12:54:44 -0500

    finished first get request, able to get users to show up on localhost 5000

--- api/server.js
+++ api/server.js
@@ -1,3 +1,48 @@
-// BUILD YOUR SERVER HERE
+//Imports
+const express = require('express');
+const Users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+
+//Instance of Express App
+const server = express();
+
+
+//Global Middleware
+server.use(express.json());//teaches express to parse request bodies as JSON//
+
+//Endpoints
+
+
+//Get Endpoints
+server.get('/', (req, res)=>{
+    console.log(`This is a ${req.method}`);
+    res.json({message:'Hey Steven!'})
+});
+
+server.get('/api/users', async (req, res)=>{
+    try {
+        const users = await Users.find()
+        if(!users){
+            res.status(404).json({
+                message: 'Users does not exist yet'
+            })
+        }else {
+            res.status(200).json(users)
+        }  
+    } catch(err) {
+        //handle error
+            res.status(500).json({
+            message: err.message,
+            customMessage: 'Something happened while getting the users'
+        })
+    }
+})
+//Get Enpoints
+
+//
+
+
+
+//Exposing the server to other modules
+module.exports = server; // EXPORT YOUR SERVER instead of {}
